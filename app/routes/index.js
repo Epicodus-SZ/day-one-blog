03f1f8da1495c899c29cafef9e080c59c18c0857
Anna Kuznetsova and Steve Zaske <anna+steve@>
2017-08-10 08:43:12 -0700

    ability to add posts to db, and create timestamp data with post creation

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,19 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-
   model() {
     return this.store.findAll('post');
   },
   actions: {
     createPost() {
       debugger;
+      var date = new Date();
+      var dMonth = date.getMonth()+1;
+      var dateString = dMonth+'/'+date.getDate()+'/'+date.getFullYear();
       var params = {
-        author: this.get('author'),
-        imgurl: this.get('inputImageUrl'),
-        text: this.get('inputText'),
-        title: this.get('inputTitle'),
-        timestamp: "June 3, 2016",
+        author: this.controller.get('author'),
+        imgurl: this.controller.get('inputImageUrl'),
+        text: this.controller.get('inputText'),
+        title: this.controller.get('inputTitle'),
+        timestamp: dateString,
       };
       var newPost = this.store.createRecord('post', params);
       newPost.save();
